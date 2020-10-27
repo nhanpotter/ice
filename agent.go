4d049907eca6cd3e8ca6641ed81bad0586e01071
Nguyen Tien Nhan <nguyentiennhan@seagroup.com>
2020-10-27 14:20:17 +0800

    add log

--- agent.go
+++ agent.go
@@ -700,6 +700,7 @@ func (a *Agent) checkKeepalive() {
 			(time.Since(selectedPair.remote.LastReceived()) > a.keepaliveInterval)) {
 		// we use binding request instead of indication to support refresh consent schemas
 		// see https://tools.ietf.org/html/rfc7675
+		a.log.Tracef("checkKeepalive: ping STUN from %s to %s", selectedPair.local.String(), selectedPair.remote.String())
 		a.selector.PingCandidate(selectedPair.local, selectedPair.remote)
 	}
 }
@@ -1097,6 +1098,7 @@ func (a *Agent) handleInbound(m *stun.Message, local Candidate, remote net.Addr)
 	}
 
 	if remoteCandidate != nil {
+		a.log.Tracef("handleInbound: remoteCandidate %s seen false", remoteCandidate.String())
 		remoteCandidate.seen(false)
 	}
 }
